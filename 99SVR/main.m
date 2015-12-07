7878543d7beee0fc69e8d63090213b388ace4052
xia zhonglin <xiazhonglin@xias-macbook-pro.local>
2015-12-07 15:50:16 +0800

    Initial Commit

---
+++ 99SVR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  99SVR
+//
+//  Created by xia zhonglin  on 12/7/15.
+//  Copyright © 2015 xia zhonglin . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
