43c51b33bb86b6a31e5f4a665302e116c33dbce0
刘海东 <446841725@qq.com>
2016-04-15 10:56:27 +0800

    高手操盘 顶部滑动控件的封装

--- 99SVR/Trader/view/SliderMenuView.h
+++ 99SVR/Trader/view/SliderMenuView.h
@@ -1,12 +1,12 @@
 
-#import <UIKit/UIKit.h>
 
+
+
+#import <UIKit/UIKit.h>
 
 @interface SliderMenuBottomScroView : UIScrollView
 
 @end
-
-
 
 @interface SliderMenuView : UIView
 
@@ -20,9 +20,12 @@
 
 
 
+
+
 - (instancetype)initWithFrame:(CGRect)frame withTitles:(NSArray *)titles;
 /**存储view的数组*/
 @property (nonatomic, strong) NSArray *viewArrays;
+/**点击顶部按钮 或者滑动切换对应页面时候的回调*/
 @property (nonatomic, copy) void (^DidSelectSliderIndex)(NSInteger index);
 
 @end
