e1cc7a67fc5769198a9d3983b51c9365da95aa49
刘海东 <446841725@qq.com>
2016-05-06 17:35:34 +0800

    修改键盘弹出时候的黑色块的bug

--- 99SVR/leftMenu/controller/BandingMobileViewController.m
+++ 99SVR/leftMenu/controller/BandingMobileViewController.m
@@ -51,7 +51,12 @@
     strDate = [fmt stringFromDate:date];
     [self.view setBackgroundColor:UIColorFromRGB(0xffffff)];
     [self createText];
-    [_txtName becomeFirstResponder];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        [_txtName becomeFirstResponder];
+    });
+    
 }
 
 - (void)createText
