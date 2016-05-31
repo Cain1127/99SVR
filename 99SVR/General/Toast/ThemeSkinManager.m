9ae705402c508d8ab9da4b0e0c3a59df643811d6
刘海东 <446841725@qq.com>
2016-05-31 17:20:08 +0800

    commit 17:20

--- 99SVR/General/Toast/ThemeSkinManager.m
+++ 99SVR/General/Toast/ThemeSkinManager.m
@@ -26,14 +26,35 @@ static ThemeSkinManager *manager = nil;
         case ThemeSkinType_Default://默认皮肤
         {
             if ([UserInfo sharedUserInfo].nStatus){
-                _titleArray = @[@"首页",@"财经直播",@"专家观点",@"高手操盘",@"我"];
-                _normalImageArray = @[@"home",@"video_live",@"tab_text_icon_normal",@"tab_operate_n",@"tab_me_p"];
-                _selectImageArray = @[@"home_h",@"video_live_h",@"tab_text_icon_pressed",@"tab_operate_h",@"tab_me_p"];
+                _titleArray = @[@"首页",
+                                @"财经直播",
+                                @"专家观点",
+                                @"高手操盘",
+                                @"我"];
+                _normalImageArray = @[@"home",
+                                      @"video_live",
+                                      @"tab_text_icon_normal",
+                                      @"tab_operate_n",
+                                      @"tab_me_p"];
+                _selectImageArray = @[@"home_h",
+                                      @"video_live_h",
+                                      @"tab_text_icon_pressed",
+                                      @"tab_operate_h",
+                                      @"tab_me_p"];
                 
             }else{
-                _titleArray = @[@"首页",@"财经直播",@"专家观点",@"我"];
-                _normalImageArray = @[@"home",@"video_live",@"tab_text_icon_normal",@"tab_me_p"];
-                _selectImageArray = @[@"home_h",@"video_live_h",@"tab_text_icon_pressed",@"tab_me_p"];
+                _titleArray = @[@"首页",
+                                @"财经直播",
+                                @"专家观点",
+                                @"我"];
+                _normalImageArray = @[@"home",
+                                      @"video_live",
+                                      @"tab_text_icon_normal",
+                                      @"tab_me_p"];
+                _selectImageArray = @[@"home_h",
+                                      @"video_live_h",
+                                      @"tab_text_icon_pressed",
+                                      @"tab_me_p"];
             }
             
             //tabbar
@@ -59,32 +80,6 @@ static ThemeSkinManager *manager = nil;
         {
             
             
-            if ([UserInfo sharedUserInfo].nStatus){
-                _titleArray = @[@"首页",@"财经直播",@"专家观点",@"高手操盘",@"我"];
-                _normalImageArray = @[@"home",@"video_live",@"tab_text_icon_normal",@"tab_operate_n",@"tab_me_p"];
-                _selectImageArray = @[@"home_h",@"video_live_h",@"tab_text_icon_pressed",@"tab_operate_h",@"tab_me_p"];
-                
-            }else{
-                _titleArray = @[@"首页",@"财经直播",@"专家观点",@"我"];
-                _normalImageArray = @[@"home",@"video_live",@"tab_text_icon_normal",@"tab_me_p"];
-                _selectImageArray = @[@"home_h",@"video_live_h",@"tab_text_icon_pressed",@"tab_me_p"];
-            }
-            
-            //tabbar
-            _tabbarBackColor = [UIColor whiteColor];
-            _tabbarMoveImageName = @"";
-            _normalItemColor =  UIColorFromRGB(0x919191);
-            _selectItemColor = UIColorFromRGB(0x0078DD);
-            _navBarColor = UIColorFromRGB(0xffffff);
-            _navBarTitColor = UIColorFromRGB(0x4C4C4C);
-            
-            //导航条
-            _navBarLBtnNImage = @"nav_menu_icon_n";
-            _navBarLBtnHImage = @"nav_menu_icon_p";
-            _navBarRBtnNImage = @"nav_search_icon_n";
-            _navBarRBtnHImage = @"nav_search_icon_p";
-            
-            //
             
             
         }
